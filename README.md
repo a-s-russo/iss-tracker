591525ce334f97b7cd480bda206c2f40484e3098
Anthony Russo <a.s.russo@outlook.com>
2024-08-07 08:14:52 +0930

    Use cache to store sunrise and sunset times

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ISS tracker
-This script will notify you via email when the International Space Station (ISS) is within 5 degrees of the latitude and longitude of a given location. Since the ISS can only be seen clearly at night, the script will only notify you when it is dark at the given location.
+This script will notify you via email when the [International Space Station (ISS)](https://www.nasa.gov/international-space-station/) is within 5 degrees of the latitude and longitude of a given location. Since the sky must be dark to view the ISS, the script will notify you only when it is night time at the given location.
 
 The script is based on API calls for the ISS' position ([http://open-notify.org/Open-Notify-API/ISS-Location-Now/](http://open-notify.org/Open-Notify-API/ISS-Location-Now/)) and for the times of sunrise and sunset at the given location ([https://sunrise-sunset.org/api](https://sunrise-sunset.org/api)).
 
@@ -9,8 +9,7 @@ Inspired by the project for day 33 of the course: [100 Days of Python](https://1
 
 1. Ensure [Python](https://www.python.org/) is installed on your computer.
 2. Download the Python script from this repository (that is, the `main.py` file) into the same folder.
-3. At the top of the script, edit your:
-   - location (latitude and longitude)
-   - email (address and password and [SMTP server name](https://sendgrid.com/en-us/blog/what-is-an-smtp-server))
-   - [time zone ID](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+3. Under the `# Define constants` section at the top of the script, edit the:
+   - location (latitude, longitude and [time zone ID](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones))
+   - email (address, password and [SMTP server name](https://sendgrid.com/en-us/blog/what-is-an-smtp-server))
 5. Run `main.py`.
--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from datetime import datetime
 
 import requests
 
+# Define constants
 MY_EMAIL = '<insert email address>'
 MY_PASSWORD = '<insert email password>'
-MY_LAT = 9999 # Insert latitude
-MY_LNG = 9999 # Insert longitude
-MY_TZ = '<insert timezone>'
+MY_LAT = 9999  # Insert latitude
+MY_LNG = 9999  # Insert longitude
+MY_TZ = '<insert time zone>'
 MY_SMTP = '<insert SMTP server name>'
+
+# Create cache to store sunrise and sunset times for each day
+cache = {}
 
 
 def is_iss_overhead():
@@ -22,47 +26,60 @@ def is_iss_overhead():
 
 
 def is_night():
-    parameters = {
-        'lat': MY_LAT,
-        'lng': MY_LNG,
-        'formatted': 0,
-        'tzid': MY_TZ,
-    }
+    global cache
 
-    # Get sunrise and sunset times
-    response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
-    response.raise_for_status()
-    data = response.json()
-
-    # Sunrise
-    sunrise_string = data['results']['sunrise']
-    sunrise_date_string = sunrise_string.split('T')[0]
-    sunrise_time_string = sunrise_string.split('T')[1].split('+')[0]
-    sunrise_datetime_string = sunrise_date_string + ' ' + sunrise_time_string
-    sunrise = datetime.strptime(sunrise_datetime_string, '%Y-%m-%d %H:%M:%S').astimezone()
-
-    # Sunset
-    sunset_string = data['results']['sunset']
-    sunset_date_string = sunset_string.split('T')[0]
-    sunset_time_string = sunset_string.split('T')[1].split('+')[0]
-    sunset_datetime_string = sunset_date_string + ' ' + sunset_time_string
-    sunset = datetime.strptime(sunset_datetime_string, '%Y-%m-%d %H:%M:%S').astimezone()
-
-    # Current time
+    # Current time and date
     now = datetime.now().astimezone()
+    date_today = now.strftime('%Y-%m-%d')
+
+    # Retrieve sunrise and sunset times from cache if available, otherwise fetch via API call
+    if date_today in cache:
+        sunrise = cache[date_today]['sunrise']
+        sunset = cache[date_today]['sunset']
+    else:
+        parameters = {
+            'lat': MY_LAT,
+            'lng': MY_LNG,
+            'formatted': 0,
+            'tzid': MY_TZ,
+        }
+
+        # Get sunrise and sunset times
+        response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
+        response.raise_for_status()
+        data = response.json()
+
+        # Sunrise
+        sunrise_string = data['results']['sunrise']
+        sunrise_date_string = sunrise_string.split('T')[0]
+        sunrise_time_string = sunrise_string.split('T')[1].split('+')[0]
+        sunrise_datetime_string = sunrise_date_string + ' ' + sunrise_time_string
+        sunrise = datetime.strptime(sunrise_datetime_string, '%Y-%m-%d %H:%M:%S').astimezone()
+
+        # Sunset
+        sunset_string = data['results']['sunset']
+        sunset_date_string = sunset_string.split('T')[0]
+        sunset_time_string = sunset_string.split('T')[1].split('+')[0]
+        sunset_datetime_string = sunset_date_string + ' ' + sunset_time_string
+        sunset = datetime.strptime(sunset_datetime_string, '%Y-%m-%d %H:%M:%S').astimezone()
+
+        # Cache times
+        times = {'sunrise': sunrise, 'sunset': sunset}
+        cache[date_today] = times
 
     return now <= sunrise or now >= sunset
 
 
 while True:
-    if is_iss_overhead() and is_night():
-        now = datetime.now().astimezone()
+    if is_night() and is_iss_overhead():
+        timestamp = datetime.now().astimezone().strftime('%Y-%m-%d %H:%M')
         with smtplib.SMTP(MY_SMTP) as connection:
             connection.starttls()
             connection.login(MY_EMAIL, MY_PASSWORD)
             connection.sendmail(
                 from_addr=MY_EMAIL,
                 to_addrs=MY_EMAIL,
-                msg=f'Subject:Look up! 👆\n\nThe ISS is above you in the sky!\nTime: {now}'
+                msg=f'Subject:Look up! \n\nThe ISS is above you in the sky!\n\nTimestamp: {timestamp}'
             )
+        break
     time.sleep(300)
